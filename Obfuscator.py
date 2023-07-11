55f9e100cfc98712656e41b1ee162401ded3f322
TheCuteOwl <114611129+TheCuteOwl@users.noreply.github.com>
2023-07-12 00:58:09 +0200

    Update Obfuscator.py

--- Obfuscator.py
+++ Obfuscator.py
@@ -1,60 +1,131 @@
-import os
-import sys
-import subprocess
-import argparse
-import random
-import time
-import marshal
-import zlib
+import random, string, base64, codecs, argparse, os, sys
+from textwrap import wrap
+from lzma import compress
+from marshal import dumps
 
+class Obfuscator:
+    def __init__(self, code, outputpath):
+        self.code = code.encode()
+        self.outpath = outputpath
+        self.varlen = 3
+        self.vars = {}
 
-def install_dependencies():
-    requirements_file = 'requirements.txt'
-    python_executable = 'python' + '.'.join(str(i) for i in sys.version_info[:2])
-    if subprocess.run([python_executable, '-m', 'pip', 'install', '-r', requirements_file]).returncode == 0:
-        print('[+] Dependencies installed. Please run the program again.')
-        sys.exit()
-    else:
-        print('[!] An error occurred while installing dependencies. Make sure pip is installed or requirements.txt is available.')
-        sys.exit()
+        self.marshal()
+        self.encrypt1()
+        self.encrypt2()
+        self.finalize()
+    
+    def generate(self, name):
+        res = self.vars.get(name)
+        if res is None:
+            res = "_" + "".join(["_" for _ in range(self.varlen)])
+            self.varlen += 1
+            self.vars[name] = res
+        return res
+    
+    def encryptstring(self, string, config= {}, func= False):
+        b64 = list(b"base64")
+        b64decode = list(b"b64decode")
+        __import__ = config.get("__import__", "__import__")
+        getattr = config.get("getattr", "getattr")
+        bytes = config.get("bytes", "bytes")
+        eval = config.get("eval", "eval")
+        if not func:
+            return f'{getattr}({__import__}({bytes}({b64}).decode()), {bytes}({b64decode}).decode())({bytes}({list(base64.b64encode(string.encode()))})).decode()'
+        else:
+            attrs = string.split(".")
+            base = self.encryptstring(attrs[0], config)
+            attrs = list(map(lambda x: self.encryptstring(x, config, False), attrs[1:]))
+            newattr = ""
+            for i, val in enumerate(attrs):
+                if i == 0:
+                    newattr = f'{getattr}({eval}({base}), {val})'
+                else:
+                    newattr = f'{getattr}({newattr}, {val})'
+            return newattr
+            
+    def encryptor(self, config):
+        def func_(string, func= False):
+            return self.encryptstring(string, config, func)
+        return func_
+    
+    def compress(self):
+        self.code = compress(self.code)
+    
+    def marshal(self):
+        self.code = dumps(compile(self.code, "<string>", "exec"))
+    
+    def encrypt1(self):
+        code = base64.b64encode(self.code).decode()
+        partlen = int(len(code)/4)
+        code = wrap(code, partlen)
+        var1 = self.generate("a")
+        var2 = self.generate("b")
+        var3 = self.generate("c")
+        var4 = self.generate("d")
+        init = [f'{var1}="{codecs.encode(code[0], "rot13")}"', f'{var2}="{code[1]}"', f'{var3}="{code[2][::-1]}"', f'{var4}="{code[3]}"']
 
+        random.shuffle(init)
+        init = ";".join(init)
+        self.code = f'''
+{init};__import__({self.encryptstring("builtins")}).exec(__import__({self.encryptstring("marshal")}).loads(__import__({self.encryptstring("base64")}).b64decode(__import__({self.encryptstring("codecs")}).decode({var1}, __import__({self.encryptstring("base64")}).b64decode("{base64.b64encode(b'rot13').decode()}").decode())+{var2}+{var3}[::-1]+{var4})))
+'''.strip().encode()
+    
+    def encrypt2(self):
+        self.compress()
+        var1 = self.generate("e")
+        var2 = self.generate("f")
+        var3 = self.generate("g")
+        var4 = self.generate("h")
+        var5 = self.generate("i")
+        var6 = self.generate("j")
+        var7 = self.generate("k")
+        var8 = self.generate("l")
+        var9 = self.generate("m")
 
-def encode(source):
-    selected_mode = random.choice((zlib,))
-    marshal_encoded = marshal.dumps(compile(source, 'Modi-Fuscate', 'exec'))
-    encoded = selected_mode.compress(marshal_encoded)
-    tmp_code = 'import marshal, zlib; exec(marshal.loads({}.decompress({})))'
-    return tmp_code.format(selected_mode.__name__, encoded)
+        conf = {
+            "getattr" : var4,
+            "eval" : var3,
+            "__import__" : var8,
+            "bytes" : var9
+        }
+        encryptstring = self.encryptor(conf)
+        
+        self.code = f'''
+{var3} = eval({self.encryptstring("eval")});{var4} = {var3}({self.encryptstring("getattr")});{var8} = {var3}({self.encryptstring("__import__")});{var9} = {var3}({self.encryptstring("bytes")});{var5} = lambda {var7}: {var3}({encryptstring("compile")})({var7}, {encryptstring("<string>")}, {encryptstring("exec")});{var1} = {self.code}
+{var2} = {encryptstring('__import__("builtins").list', func= True)}({var1})
+try:
+    {encryptstring('__import__("builtins").exec', func= True)}({var5}({encryptstring('__import__("lzma").decompress', func= True)}({var9}({var2})))) or {encryptstring('__import__("os")._exit', func= True)}(0)
+except {encryptstring('__import__("lzma").LZMAError', func= True)}:...
+'''.strip().encode()
 
-def parse_args():
-    parser = argparse.ArgumentParser(description='Obfuscate Python programs')
-    parser.add_argument('-r', '--recursion', dest='r', action='store_true',
-                        help='Recursion encoding. Provides stronger obfuscation.')
-    parser.add_argument('-i', '--input', type=str, help='Input file name', required=True)
-    parser.add_argument('-o', '--output', type=str, help='Output file name', required=True)
-    parser.add_argument('-s', '--strength', type=int, help='Strength of the obfuscation. 100 recommended', required=True)
-    return parser.parse_args()
+    def encrypt3(self):
+        self.compress()
+        data = base64.b64encode(self.code)
+        self.code = f'import base64, lzma; exec(compile(lzma.decompress(base64.b64decode({data})), "<string>", "exec"))'.encode()
 
+    def finalize(self):
+        if os.path.dirname(self.outpath).strip() != "":
+            os.makedirs(os.path.dirname(self.outpath), exist_ok= True)
+        with open(self.outpath, "w") as e:
+            e.write(self.code.decode())
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(prog= sys.argv[0], description= "Obfuscates python program to make it harder to read")
+    parser.add_argument("FILE", help= "Path to the file containing the python code")
+    parser.add_argument("-o", type= str, help= 'Output file path [Default: "Obfuscated_<FILE>.py"]', dest= "path")
+    args = parser.parse_args()
 
-def main():
-    args = parse_args()
-    print(f'[+] Encoding {args.input} Please be patient...')
-    if not args.r:
-        print('[!] You have not selected recursion mode.')
-    with open(args.input, encoding='utf-8') as input_file:
-        encoded = input_file.read()
-        for _ in range(args.strength):
-            encoded = encode(source=encoded) if args.r else encode(source=encode(source=encoded))
-            time.sleep(0.1)
-    with open(args.output, 'w') as output:
-        output.write(f"try:\n\t{encoded}\nexcept KeyboardInterrupt:\n\tpass")
-    print(f'[+] Encoding successful!\nSaved as {args.output}')
-    input('Press Any key to quit')
-
-
-if __name__ == '__main__':
-    try:
-        import requests
-    except ModuleNotFoundError:
-        install_dependencies()
-    main()
+    if not os.path.isfile(sourcefile := args.FILE):
+        print(f'No such file!')
+        os._exit(1)
+    elif not sourcefile.endswith((".py", ".pyw")):
+        print('The file does not have a valid python script extention!')
+        os._exit(1)
+    
+    if args.path is None:
+        args.path = "Obfuscated_" + os.path.basename(sourcefile)
+    
+    with open(sourcefile, encoding='utf8') as sourcefile:
+        code = sourcefile.read()
+    
+    Obfuscator(code, args.path)
