a027e69a798ee0aa2644447504c33166ce68bc9d
TheCuteOwl <114611129+TheCuteOwl@users.noreply.github.com>
2023-09-27 20:18:25 +0200

    Update obfuscator.py

--- obfuscator.py
+++ obfuscator.py
@@ -82,4 +82,4 @@ s = base64.b64encode(obfuscated_code.encode('utf-8'))
 with open(f'.\\build\{ss}', "wb") as obfu_file:
     obfu_file.write(f"{all_fake_code};import base64,subprocess\ntry:from cryptography.fernet import Fernet\nexcept:subprocess.run('python -m pip install cryptography', shell=True)\n{all_fake_code}\n{e} = exec\n{all_fake_code}\nb={s}.decode('utf-8')\n{e}(base64.b64decode(b))\n{all_fake_code}".encode("utf-8"))   
 os.remove("encryption_key.txt")
-print(f"The code has been encrypted, Filename: .\\build\{ss}")
+input(f"The code has been encrypted, Filename: .\\build\{ss}")
